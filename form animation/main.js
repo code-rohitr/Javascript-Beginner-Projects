8cf588bd0e9ab2ea8c30f7d52dc8149169d11bf9
rohit raj <official.rohitr@gmail.com>
2021-06-18 13:35:33 +0530

    added form animation

---
+++ form animation/main.js
@@ -0,0 +1,49 @@
+// const input = document.querySelector(".input");
+// const label = document.querySelector(".label");
+
+
+// input.addEventListener("focus", animateSpan)
+// input.addEventListener("blur", animateSpan)
+// input.addEventListener("click", ()=>{
+
+//     label.classList.add("animate");
+// })
+
+// function animateSpan()
+// {
+
+//     if(input.value === '')
+//     {
+//         label.classList.remove("animate");
+//     }
+//     else
+//     {
+//         label.classList.add("animate");
+//     }
+// }
+
+
+
+// const input = document.querySelector(".input");
+// const label = document.querySelectorAll(".label");
+
+// label.forEach(Element => {
+
+//     input.addEventListener("focus", animateSpan)
+//     input.addEventListener("blur", animateSpan)
+//     input.addEventListener("click", () => {
+
+//         Element.classList.add("animate");
+//     })
+
+//     function animateSpan() {
+
+//         if (input.value === '') {
+//             Element.classList.remove("animate");
+//         }
+//         else {
+//             Element.classList.add("animate");
+//         }
+//     }
+
+// })
